3635e9721a779559434cdbb81402f11cd7d19563
James M. Allen <james.m.allen@gmail.com>
2012-08-14 11:37:20 -0400

    Indentation cleanup

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,18 +2,21 @@
 cd "$(dirname "$0")"
 git pull
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
+    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
     # load/update vundle
-    vim +BundleClean! +BundleInstall! +quitall
+    if [ ! -d ~/.vim/bundle/vundle ]; then
+        git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+    fi
+    vim +BundleClean! +BundleInstall +quitall
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
+    doIt
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-	fi
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        doIt
+    fi
 fi
 unset doIt
 
@@ -21,7 +24,7 @@ unset doIt
 
 
 if [ -f ~/.bash_profile ]; then
-	source ~/.bash_profile
+    source ~/.bash_profile
 fi
 
 
